e33a5cd26524519c1cdc6f438e49eb0e393a8e81
tjibbe.hofkamp@gmail.com <tjibbe.hofkamp@gmail.com@d3bf82d2-0c16-e458-5408-27be57bd1276>
2011-12-20 20:57:12 +0000

    --change reformat xml

--- branches/rework-rcd/rcd/creatercd.py
+++ branches/rework-rcd/rcd/creatercd.py
@@ -1,8 +1,9 @@
 import Image
 import xml.dom.minidom
 
+# invoer voor dit commando, het moet eigenlijk via de command line, maar voor te testen is dit handiger
 fname = "freerct.xml"
-tag = "direction"
+tag = "foundation"
 
 class gameblock(object):
     def __init__(self,magic,version):
@@ -12,16 +13,62 @@ class gameblock(object):
         self.version = version
 
     def addimage(self,im):
+        """
+        Add an sprite image to the gameblock collection
+        """
         self.images.append(im)
 
-    def addimages(self,im,width,height,first = [0],second = [0]):
+    def addimages(self,im,width,height,first,second):
+        """
+        Add multiple sprites from one image.
+        On every permutation of a first and second coordinates
+        There will be len(first)*len(second) sprites added
+        """
+        #print "addimages",width,height,first,second
         for i in first:
             for j in second:
-                addimage(im.crop((first,second,first+width,second+height)))
+                self.addimage(im.crop((i,j,i+width,j+height)))
                 
     def addparam(self,typ,val):
+        """
+        Add a parameter to the gameblock
+        This could represent the zoom-level, or type of width, used in conjunction with this collection of sprites
+        typ can be: uint8 int8 uint16 int16 int32
+        """
         self.params.append((typ,val))
-                
+
+    def LoadFromDOM(self,domnode):
+        """
+        Fill the gameblock from a DOM object
+        """
+        for node in domnode:
+            if node.nodeType == node.ELEMENT_NODE:
+                if node.nodeName == "addimages":
+                    img = Image.open(node.getAttribute("fname"))
+                    xorg = int(node.getAttribute("xorigin"))
+                    yorg = int(node.getAttribute("yorigin"))
+                    xstep = int(node.getAttribute("xstep"))
+                    ystep = int(node.getAttribute("ystep"))
+                    cols = int(node.getAttribute("colums"))
+                    rows = int(node.getAttribute("rows"))
+                    self.addimages(img,xstep,ystep,range(xorg,xorg+xstep*cols,xstep),range(yorg,yorg+ystep*rows,ystep))
+                                                    # width,height moet er nog bij ipv xstep/ystep
+                                                    # en ook de richting hor/vert
+                elif node.nodeName == "addimage":
+                    self.addimage(Image.open(node.getAttribute("fname")))
+                elif node.nodeName == "attribute":
+                    self.addparam(node.getAttribute("type"),node.getAttribute("value"))
+                else:
+                    print "unknown tag found",node.nodename,"ignored"
+                    
+    def SaveToFile(self):
+        print "write header",self.magic,self.version
+        print "size...."
+        for i in self.params:
+            print i[0],i[1]
+        for i in self.images:
+            print "write sprite"
+        
 dom = xml.dom.minidom.parse(fname)
 
 class rcdfile(object):
@@ -30,36 +77,24 @@ class rcdfile(object):
         self.fname = fdom.getAttribute("fname")
         self.magic = fdom.getAttribute("magic")
         self.version = fdom.getAttribute("version")
+                
+    def LoadFromDOM(self,fdom):
         gbdom = fdom.getElementsByTagName("gameblock")
         for i in gbdom:
             g = gameblock(i.getAttribute("magic"),i.getAttribute("version"))
-            for node in i.childNodes:
-                if node.nodeType == 1:
-                    if node.nodeName == "addimages":
-                        img = Image.open(node.getAttribute("fname"))
-                        xorg = node.getAttribute("xorigin")
-                        yorg = node.getAttribute("yorigin")
-                        xstep = node.getAttribute("xstep")
-                        ystep = node.getAttribute("ystep")
-                        cols = node.getAttribute("colums")
-                        rows = node.getAttribute("rows")
-                        g.addimages(img,xstep,ystep,range(xorg,xorg+xstep*cols,xstep),range(yorg,yorg+ystep*rows,ystep))
-                                                        # width,height moet er nog bij ipv xstep/ystep
-                                                        # en ook de richting hor/vert
-                    elif node.nodeName == "addimage":
-                        g.addimage(Image.open(node.getAttribute("fname"))
-                    elif node.nodeName == "attribute":
-                        g.addparam(node.getAttribute("type"),node.getattribute("value"))
-                    else:
-                        print "unknown tag found",node.nodename
+            g.LoadFromDOM(i.childNodes)
             self.gameblocks.append(g)
-
-    def handleimages(fname,xorg,yorg,xstep,ystep,rows,cols,width = xstep,height = ystep):
+            
+    def SaveToFile(self):
+        print "open",self.fname,"for write"
+        print "write header magic + version"
+        for i in self.gameblocks:
+            i.SaveToFile()
                     
 fdom = dom.getElementsByTagName("file")
 for f in fdom:
-    print f.getAttribute("id")
+    #print f.getAttribute("id")
     if f.getAttribute("id") == tag:
         r = rcdfile(f)
-        
-
+        r.LoadFromDOM(f)
+        r.SaveToFile()
